0421c19b973d899125136b88859d92dad2c2e96c
Nico Mexis <nico.mexis@kabelmail.de>
2026-05-24 09:22:56 +0200

    Fix ItemStack deletion
    Thanks to plush nei for the background info on what needs to be fixed exactly

---
+++ src/main/java/de/femtopedia/nil125fix/nei/FRHSPTransformer.java
@@ -0,0 +1,23 @@
+package de.femtopedia.nil125fix.nei;
+
+import de.femtopedia.nil125fix.MiniPlusTransformer;
+import nilloader.api.lib.mini.PatchContext;
+import nilloader.api.lib.mini.annotation.Patch;
+
+@Patch.Class("codechicken.nei.FurnaceRecipeHandler$SmeltingPair")
+public class FRHSPTransformer extends MiniPlusTransformer {
+
+    @Patch.Method("<init>(Lcodechicken/nei/FurnaceRecipeHandler;Lnet/minecraft/src/ItemStack;Lnet/minecraft/src/ItemStack;)V")
+    public void initFRHSP(PatchContext ctx) {
+        ctx.search(
+                ALOAD(2)
+        ).jumpAfter();
+
+        ctx.add(
+                INVOKEVIRTUAL("net/minecraft/src/ItemStack", "copy", "()Lnet/minecraft/src/ItemStack;"),
+                ASTORE(1),
+                ALOAD(1)
+        );
+    }
+
+}
