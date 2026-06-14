de2a6cb8a2a26b123ba8320613a07ed6e02a019a
Nico Mexis <nico.mexis@kabelmail.de>
2026-06-14 21:36:16 +0200

    Fix Tropicraft turtle ID

---
+++ src/main/java/de/femtopedia/nil125fix/tropicraft/fixturtleid/mod_tropicraftTransformer.java
@@ -0,0 +1,22 @@
+package de.femtopedia.nil125fix.tropicraft.fixturtleid;
+
+import de.femtopedia.nil125fix.MiniPlusTransformer;
+import nilloader.api.lib.mini.PatchContext;
+import nilloader.api.lib.mini.annotation.Patch;
+
+@Patch.Class("mod_tropicraft")
+public class mod_tropicraftTransformer extends MiniPlusTransformer {
+
+    @Patch.Method("addAllEntities()V")
+    public void warnIfAlreadyInMap(PatchContext ctx) {
+        ctx.search(
+                LDC("Turtle")
+        ).jumpAfter();
+
+        ctx.add(
+                POP(),
+                LDC("TurtleTC")
+        );
+    }
+
+}
