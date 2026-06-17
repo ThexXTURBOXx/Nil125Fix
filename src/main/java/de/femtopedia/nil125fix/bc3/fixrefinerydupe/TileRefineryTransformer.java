7865301e6aa06d86e4303392d2905e704f33151f
Nico Mexis <nico.mexis@kabelmail.de>
2026-06-17 17:02:30 +0200

    Fix refinery dupe
    Fixes #2

---
+++ src/main/java/de/femtopedia/nil125fix/bc3/fixrefinerydupe/TileRefineryTransformer.java
@@ -0,0 +1,59 @@
+package de.femtopedia.nil125fix.bc3.fixrefinerydupe;
+
+import de.femtopedia.nil125fix.MiniPlusTransformer;
+import nilloader.api.lib.asm.tree.LabelNode;
+import nilloader.api.lib.mini.PatchContext;
+import nilloader.api.lib.mini.annotation.Patch;
+
+@Patch.Class("buildcraft.factory.TileRefinery")
+public class TileRefineryTransformer extends MiniPlusTransformer {
+
+    @Patch.Method.AffectsControlFlow
+    @Patch.Method("consumeInput(Lbuildcraft/api/liquids/LiquidStack;)Z")
+    public void compareQuantities(PatchContext ctx) {
+        LabelNode Lskip = new LabelNode();
+
+        ctx.search(
+                INVOKEVIRTUAL("buildcraft/api/liquids/LiquidStack", "isLiquidEqual", "(Lbuildcraft/api/liquids/LiquidStack;)Z"),
+                IFEQ(null)
+        ).jumpAfter();
+        ctx.add(
+                ALOAD(0),
+                GETFIELD("buildcraft/factory/TileRefinery", "slot1", "Lbuildcraft/factory/TileRefinery$Slot;"),
+                GETFIELD("buildcraft/factory/TileRefinery$Slot", "quantity", "I"),
+                ALOAD(1),
+                GETFIELD("buildcraft/api/liquids/LiquidStack", "amount", "I"),
+                IF_ICMPLT(Lskip)
+        );
+
+        ctx.search(
+                IRETURN()
+        ).jumpAfter();
+        ctx.add(
+                Lskip
+        );
+
+        Lskip = new LabelNode();
+
+        ctx.search(
+                INVOKEVIRTUAL("buildcraft/api/liquids/LiquidStack", "isLiquidEqual", "(Lbuildcraft/api/liquids/LiquidStack;)Z"),
+                IFEQ(null)
+        ).jumpAfter();
+        ctx.add(
+                ALOAD(0),
+                GETFIELD("buildcraft/factory/TileRefinery", "slot2", "Lbuildcraft/factory/TileRefinery$Slot;"),
+                GETFIELD("buildcraft/factory/TileRefinery$Slot", "quantity", "I"),
+                ALOAD(1),
+                GETFIELD("buildcraft/api/liquids/LiquidStack", "amount", "I"),
+                IF_ICMPLT(Lskip)
+        );
+
+        ctx.search(
+                IRETURN()
+        ).jumpAfter();
+        ctx.add(
+                Lskip
+        );
+    }
+
+}
